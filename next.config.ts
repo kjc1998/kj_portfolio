2e7e701a9a31063b56378082c5acf56e8e2f9237
Kai Jie Chow <kj1chow@gmail.com>
2025-07-04 14:05:10 +0100

    implement next export functionality

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	output: 'export',
+	/* only valid for absolut URL (CDN cases) */
+	// assetPrefix: './',
 };
 
 export default nextConfig;
