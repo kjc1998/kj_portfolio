15d5b4e829f233362058eb96d57aea4ccfd1b218
Kai Jie Chow <kj1chow@gmail.com>
2026-04-11 13:28:59 +0100

    add project tsx

---
+++ app/models/project.tsx
@@ -0,0 +1,30 @@
+'use-client';
+
+import { StaticImageData } from 'next/image';
+import React from 'react';
+
+interface ProjectI {
+	name: string;
+	startDate: Date;
+	endDate: Date | null;
+	link: string | null;
+	image: StaticImageData;
+	status: 'ongoing' | 'done';
+	description: string;
+	tags: string[];
+	others: React.JSX.Element | null;
+}
+
+interface HighlightI {
+	start: number;
+	end: number;
+}
+
+interface ProjectHighlightI {
+	name: HighlightI[];
+	status: HighlightI[];
+	description: HighlightI[];
+	tags: HighlightI[][];
+}
+
+export type { HighlightI, ProjectHighlightI, ProjectI };
