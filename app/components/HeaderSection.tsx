b3c61c5ba20be715b0fd1b3401bfcf1c483fb5dc
Kai Jie Chow <kj1chow@gmail.com>
2026-04-19 12:52:03 +0100

    improve header css

--- app/components/HeaderSection.tsx
+++ app/components/HeaderSection.tsx
@@ -2,14 +2,10 @@ import { Section } from '../models/enums';
 
 const HeaderSection = () => {
 	return (
-		<section className="sticky top-4 z-10 mx-auto w-[92%] sm:w-fit px-4 sm:px-5 py-2 bg-gray-700/70 backdrop-blur-md rounded-full shadow-lg border border-white/5">
+		<section className="sticky top-4 z-10 mx-auto w-[92%] sm:w-fit px-4 sm:px-5 py-2 bg-gray-700/5 backdrop-blur-md rounded-full shadow-lg">
 			<nav className="flex justify-center flex-wrap gap-3 sm:gap-6">
 				{Object.values(Section).map((value, index) => (
-					<a
-						key={index}
-						href={'#' + value}
-						className="px-4 py-1.5 text-sm font-medium rounded-full hover:bg-white hover:text-gray-500 hover:shadow-sm transition-all duration-200"
-					>
+					<a key={index} href={'#' + value} className="px-4 py-1.5 text-lg rounded-full hover:bg-white hover:text-gray-500 hover:shadow-sm transition-all duration-200">
 						{value}
 					</a>
 				))}
